b24a51e63e31fe622e4e0fdfd23ea5290b339c97
ViktoriiaViktoriia <viktoriatsch@gmail.com>
2025-03-03 22:47:23 +0200

    Added a data visualization function to plot top 5 products by sales

--- src/data_visualization.py
+++ src/data_visualization.py
@@ -152,3 +152,86 @@ def plot_regional_sales_by_year(
     except Exception as e:
         logger.exception(f"An error occurred while creating the sales plot: {e}")
         return None
+
+
+def plot_top_products(
+        df: pd.DataFrame,
+        products_column: str,
+        date_column: str,
+        country_column: str,
+        sales_column: str,
+        time_period: str) -> Union[go.Figure, None]:
+    """
+    Plot top five best-selling products using Plotly.
+
+    Args:
+        df (pd.DataFrame): Cleaned data.
+        products_column (str): Column name for products.
+        date_column (str): The column representing the date.
+        country_column (str): The column representing the region (country).
+        sales_column (str): Column name for sales.
+        time_period (str): The time period for aggregation: year ("Y"), quarter ("Q"), month ("M"), or day ("D").
+
+    Returns:
+        go.Figure, None: Plot appears or nothing.
+    """
+    try:
+        logger.info("Plotting bestsellers ...")
+
+        # Check if required columns exist in DataFrame
+        missing_columns = [col for col in [sales_column, country_column, date_column, products_column]
+                           if col not in df.columns]
+        if missing_columns:
+            logger.error("Required columns not found in DataFrame.")
+            raise
+
+        # Ensure the date_column is in datetime format
+        df[date_column] = pd.to_datetime(df[date_column], errors="raise")
+
+        # Aggregate sales by date
+        df["selected_time_period"] = df[date_column].dt.to_period(time_period).astype(str)
+
+        # Group by Product, Country, and Date, summing up the sales
+        bestsellers = (
+            df.groupby([products_column, country_column, "selected_time_period"])[sales_column]
+            .sum()
+            .reset_index()
+        )
+
+        # Sort best-selling products in descending order and get the top 5
+        top_5_products = bestsellers.sort_values(sales_column, ascending=False).head(5)
+
+        # Display the results
+        print(top_5_products)
+
+        # Create a bar chart
+        fig = px.bar(
+            top_5_products,
+            x=top_5_products[products_column],
+            y=top_5_products[sales_column],
+            color=top_5_products[country_column],
+            title="Top 5 Best-Selling Products",
+            text=top_5_products[date_column]
+        )
+
+        # Customize hover template
+        fig.update_traces(
+            hovertemplate="Product = %{x}<br>Sales = %{y}<br>Date = %{text}<extra></extra>"
+        )
+
+        # Update layout
+        fig.update_layout(
+            title={
+                "text": "Top 5 Best-Selling Products",
+                "x": 0.5,  # Center the title
+                "xanchor": "center",
+                "yanchor": "top"
+            }
+        )
+
+        logger.info("Top 5 products by sales plotted successfully.")
+        return fig
+
+    except Exception as e:
+        logger.error(f"Error in plot_top_products: {e}")
+        return None
